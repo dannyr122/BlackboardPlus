8345b253e9eae19776003511457778a5687868c0
dannyr122 <dannyr151617@gmail.com>
2024-11-13 13:34:47 -0500

    add all shadcn components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layouts/app-layout";
+import Courses from "./pages/courses";
+import LandingPage from "./pages/landing";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/courses",
+        element: <Courses />,
+      },
+    ],
+  },
+]);
 
 function App() {
-
-
-  return (
-    <>
-      
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
